7f2ac462e50f60d0e6bade5b4532327bec5eba25
felipebma <32962329+felipebma@users.noreply.github.com>
2019-04-30 21:53:59 -0300

    Add files via upload

--- Arduino/sketch_apr22a/sketch_apr22a.ino
+++ Arduino/sketch_apr22a/sketch_apr22a.ino
@@ -1,7 +1,3 @@
-
-
-
-
 /*
    23/05/2018
    Bruno Felipe Corte
@@ -33,12 +29,18 @@ const char* password = "IOT2017IBM";
 
 //D4 only for Lolin board
 #define LED_BUILTIN D4
+#define S0 D0
+#define S1 D1
+#define S2 D2
+#define S3 D3
+#define sensorOut D8
+
 
 
 //Atualize os valores de Org, device type, device id e token
-#define ORG "9tla6x"
+#define ORG "qc8dss"
 #define DEVICE_TYPE "nodemcu"
-#define DEVICE_ID "nodemcuplanta01"
+#define DEVICE_ID "nodemcu14"
 #define TOKEN "12345678"
 
 
@@ -60,6 +62,18 @@ PubSubClient client(server, 1883, NULL, wifiClient);
 
 void setup() {
   Serial.begin(115200);
+  
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  
+  pinMode(sensorOut, INPUT);
+
+  //Definindo a escala de frenquencia para 20%
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,LOW);
+  
   Serial.println();
   Serial.println("Iniciando...");
 
@@ -84,39 +98,52 @@ void setup() {
 //Retorno: umidade percentual (0-100)
 //Observação: o ADC do NodeMCU permite até, no máximo, 3.3V. Dessa forma,
 //            para 3.3V, obtem-se (empiricamente) 978 como leitura de ADC
-float FazLeituraUmidade(void)
-{
-    int ValorADC;
-    float UmidadePercentual;
- 
-     ValorADC = analogRead(0);   //978 -> 3,3V
-     Serial.print("[Leitura ADC] ");
-     Serial.println(ValorADC);
- 
-     //Quanto maior o numero lido do ADC, menor a umidade.
-     //Sendo assim, calcula-se a porcentagem de umidade por:
-     //      
-     //   Valor lido                 Umidade percentual
-     //      _    0                           _ 100
-     //      |                                |   
-     //      |                                |   
-     //      -   ValorADC                     - UmidadePercentual 
-     //      |                                |   
-     //      |                                |   
-     //     _|_  978                         _|_ 0
-     //
-     //   (UmidadePercentual-0) / (100-0)  =  (ValorADC - 978) / (-978)
-     //      Logo:
-     //      UmidadePercentual = 100 * ((978-ValorADC) / 978)  
-     
-     UmidadePercentual = 100 * ((978-(float)ValorADC) / 978);
-     Serial.print("[Umidade Percentual] ");
-     Serial.print(UmidadePercentual);
-     Serial.println("%");
- 
-     return UmidadePercentual;
-}
 
+int getColor(int rgb){
+  int freq = 0;
+  if(rgb==0){
+    digitalWrite(S2,LOW);
+    digitalWrite(S3,LOW);
+  }else if(rgb==1){
+    digitalWrite(S2,HIGH);
+    digitalWrite(S3,HIGH);
+  }else{
+    digitalWrite(S2,LOW);
+    digitalWrite(S3,HIGH);
+  }
+
+  freq = pulseIn(sensorOut,LOW);
+  return freq;
+}
+//Setando os parâmetros e limites de cada letra (RGB)
+int a[] = {32,44,205,230,300,320};
+int b[] = {32,44,205,230,300,320};
+int e[] = {32,44,205,230,300,320};
+int i[] = {32,44,205,230,300,320};
+int n[] = {32,44,205,230,300,320};
+int l[] = {32,44,205,230,300,320};
+
+
+String getCharacter(void){
+  int redFrequency = getColor(0);
+  int greenFrequency = getColor(1);
+  int blueFrequency = getColor(2);
+  if(redFrequency>=a[0] && redFrequency<=a[1] && greenFrequency>=a[2] && greenFrequency<=a[3] && blueFrequency>=a[4] && blueFrequency<=a[5]){
+    return "a";
+  }else if(redFrequency>=b[0] && redFrequency<=b[1] && greenFrequency>=b[2] && greenFrequency<=b[3] && blueFrequency>=b[4] && blueFrequency<=b[5]){
+    return "b";
+  }else if(redFrequency>=e[0] && redFrequency<=e[1] && greenFrequency>=e[2] && greenFrequency<=e[3] && blueFrequency>=e[4] && blueFrequency<=e[5]){
+    return "e";
+  }else if(redFrequency>=i[0] && redFrequency<=i[1] && greenFrequency>=i[2] && greenFrequency<=i[3] && blueFrequency>=i[4] && blueFrequency<=i[5]){
+    return "i";
+  }else if(redFrequency>=n[0] && redFrequency<=n[1] && greenFrequency>=n[2] && greenFrequency<=n[3] && blueFrequency>=n[4] && blueFrequency<=n[5]){
+    return "n";
+  }else if(redFrequency>=l[0] && redFrequency<=l[1] && greenFrequency>=l[2] && greenFrequency<=l[3] && blueFrequency>=l[4] && blueFrequency<=l[5]){
+    return "l";
+  }
+  return "";
+  
+}
 
 void loop() {
 
@@ -131,7 +158,10 @@ void loop() {
     Serial.println();
   }
 
+  String letter = getCharacter();
 
+
+/*
   float umidade = FazLeituraUmidade();
   
   // Conversao Floats para Strings
@@ -142,12 +172,12 @@ void loop() {
   dtostrf(umidade, 2, 1, TempString);
   String umidadestr =  String(TempString);
 
-
+*/
   // Prepara JSON para IOT Platform
   int length = 0;
 
 
-  String payload = "{\"d\":{\"umidade\":\"" + umidadestr + "\"}}";
+  String payload = "{\"d\":{\"letra\":\" Luana chatona \"}}";
 
 
   length = payload.length();
